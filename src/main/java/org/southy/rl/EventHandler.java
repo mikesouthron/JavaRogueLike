b8dd1c4c0534827091088f6bbaf464d3811365e1
Mike Southron <mike.soutron@gmail.com>
2022-07-16 21:26:31 +0100

    Key handling

--- src/main/java/org/southy/rl/EventHandler.java
+++ src/main/java/org/southy/rl/EventHandler.java
@@ -3,9 +3,35 @@ package org.southy.rl;
 import org.southy.rl.gen.Procgen;
 
 import java.awt.event.KeyEvent;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 public class EventHandler {
+
+    static class Direction {
+        public int x;
+        public int y;
+
+        public Direction(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private static final Map<Integer, Direction> MOVE_KEYS = Map.of(
+            KeyEvent.VK_NUMPAD4, new Direction(-1, 0),
+            KeyEvent.VK_NUMPAD6, new Direction(1, 0),
+            KeyEvent.VK_NUMPAD8, new Direction(0, -1),
+            KeyEvent.VK_NUMPAD2, new Direction(0, 1),
+            KeyEvent.VK_NUMPAD3, new Direction(1, 1),
+            KeyEvent.VK_NUMPAD1, new Direction(-1, 1),
+            KeyEvent.VK_NUMPAD7, new Direction(-1, -1),
+            KeyEvent.VK_NUMPAD9, new Direction(1, -1)
+    );
+
+    private static final Set<Integer> WAIT_KEYS = Set.of(KeyEvent.VK_NUMPAD5, KeyEvent.VK_PERIOD, KeyEvent.VK_CLEAR);
+
 
     Engine engine;
     Logger logger;
@@ -31,35 +57,25 @@ public class EventHandler {
 
         var player = engine.player;
 
-        switch (event.getKeyCode()) {
-            case 100:
-                return Optional.of(new BaseAction.BumpAction(player, -1, 0, event.isShiftDown()));
-            case 102:
-                return Optional.of(new BaseAction.BumpAction(player, 1, 0, event.isShiftDown()));
-            case 104:
-                return Optional.of(new BaseAction.BumpAction(player, 0, -1, event.isShiftDown()));
-            case 98:
-                return Optional.of(new BaseAction.BumpAction(player, 0, 1, event.isShiftDown()));
-            case 99:
-                return Optional.of(new BaseAction.BumpAction(player, 1, 1, event.isShiftDown()));
-            case 97:
-                return Optional.of(new BaseAction.BumpAction(player, -1, 1, event.isShiftDown()));
-            case 103:
-                return Optional.of(new BaseAction.BumpAction(player, -1, -1, event.isShiftDown()));
-            case 105:
-                return Optional.of(new BaseAction.BumpAction(player, 1, -1, event.isShiftDown()));
-            case 27:
-                return Optional.of(new BaseAction.EscapeAction(player));
-            case 82:
-                if (event.isControlDown()) {
-                    player.gameMap = engine.gameMap = Procgen.generateDungeon(engine, Application.maxRooms, Application.roomMinSize, Application.roomMaxSize, Application.mapWidth, Application.mapHeight, Application.maxMonstersPerRoom);
-                }
-                return Optional.empty();
-            default: {
-                logger.log("Key Pressed: " + event.getKeyCode());
-                return Optional.empty();
-            }
+        if (MOVE_KEYS.containsKey(event.getKeyCode())) {
+            var dir = MOVE_KEYS.get(event.getKeyCode());
+            return Optional.of(new BaseAction.BumpAction(player, dir.x, dir.y, event.isShiftDown()));
         }
+
+        if (WAIT_KEYS.contains(event.getKeyCode())) {
+            return Optional.of(new BaseAction.WaitAction(player));
+        }
+
+        //TODO: Action for redraw?
+        if (event.isControlDown() && event.getKeyCode() ==KeyEvent.VK_R) {
+            player.gameMap = engine.gameMap = Procgen.generateDungeon(engine, Application.maxRooms, Application.roomMinSize, Application.roomMaxSize, Application.mapWidth, Application.mapHeight, Application.maxMonstersPerRoom);
+        }
+
+        if (event.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            return Optional.of(new BaseAction.EscapeAction(player));
+        }
+
+        return Optional.empty();
     }
 
 }
